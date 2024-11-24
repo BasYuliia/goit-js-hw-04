b0fbab80a76b066780f202a3057b7501e44ee8b8
Yuliia Bas <basyuliia2017@gmail.com>
2024-11-24 19:47:42 +0200

    type

--- js/task-2.js
+++ js/task-2.js
@@ -13,17 +13,17 @@ const calcAverageCalories = function (days) {
   return totalCalories / days.length;
 };
 
-console.log(
-  calcAverageCalories([
-    { day: 'monday', calories: 3010 },
-    { day: 'tuesday', calories: 3200 },
-    { day: 'wednesday', calories: 3120 },
-    { day: 'thursday', calories: 2900 },
-    { day: 'friday', calories: 3450 },
-    { day: 'saturday', calories: 3280 },
-    { day: 'sunday', calories: 3300 },
-  ])
-); // 3180
+// console.log(
+//   calcAverageCalories([
+//     { day: 'monday', calories: 3010 },
+//     { day: 'tuesday', calories: 3200 },
+//     { day: 'wednesday', calories: 3120 },
+//     { day: 'thursday', calories: 2900 },
+//     { day: 'friday', calories: 3450 },
+//     { day: 'saturday', calories: 3280 },
+//     { day: 'sunday', calories: 3300 },
+//   ])
+// ); // 3180
 
 // console.log(
 //   calcAverageCalories([
